4d9cbfb6f3a342af3f3e31aa259376fcb0d1f636
Aleh <32686077+Gonokokk@users.noreply.github.com>
2018-02-25 15:26:12 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+  var aReg = /^\-?\d+/;
+        bRegf = /\^\d.\d*/;
+        bRegs = /\-?\d*$/;
+        cReg = /\-?\d*$/;
+        result = [];
+    var d, x1, x2; 
+        RegEx = /\s*/g;
+        equation = equation.replace(RegEx, "");
+    
+        a = Number(aReg.exec(equation));
+        b = bRegf.exec(equation); 
+        b = Number(bRegs.exec(b));      
+        c = Number(cReg.exec(equation));
+        
+        d = b*b - 4*a*c;
+
+        x1 = Math.round((-b + Math.sqrt(d))/(2*a));
+        x2 = Math.round((-b - Math.sqrt(d))/(2*a));
+        result.push(x1, x2);
+        result.sort(function(a, b) {
+        	return a - b;
+        });
+        
+        return result;
 }
